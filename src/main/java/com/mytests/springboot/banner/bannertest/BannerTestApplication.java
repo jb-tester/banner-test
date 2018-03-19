fff9b97dd4b943bc459a43911ad0ce967247cebb
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-03-19 14:20:06 +0300

    add all

---
+++ src/main/java/com/mytests/springboot/banner/bannertest/BannerTestApplication.java
@@ -0,0 +1,12 @@
+package com.mytests.springboot.banner.bannertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannerTestApplication.class, args);
+	}
+}
